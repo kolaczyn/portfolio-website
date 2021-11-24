c00fd24a1eece46bde428ed2bbfb67726cc7eb9d
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-11-24 22:16:38 +0100

    add basic footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,15 +1,55 @@
+import AppLink from "../common/AppLink";
 import ContainerBig from "../container/ContainerBig";
+import { AiFillGithub as GithubIcon } from "react-icons/ai";
+import { AiOutlineMail as MailIcon } from "react-icons/ai";
+import { FiTwitter as TwitterIcon } from "react-icons/fi";
+import AppExternalLink from "../common/AppExternalLink";
 
 type Props = {};
 
 const Footer: React.FC<Props> = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <ContainerBig className="bg-gray-800 py-2">
-      <footer className="flex justify-around">
+    <footer>
+      <div className="bg-gray-800 h-1" />
+      <ContainerBig className="bg-gray-900 py-2 flex">
+        <div className="flex-1">
+          <h4 className="uppercase font-bold">Sitemap</h4>
+          <ul className="space-x-3">
+            <AppLink to="/about-me">about me</AppLink>
+            <AppLink to="/projects">projects</AppLink>
+            <AppLink to="/light-mode">dark mode</AppLink>
+          </ul>
+        </div>
+        <div className="flex-1">
+          <h4 className="uppercase font-bold">Links</h4>
+          <ul className="space-x-3">
+            <AppExternalLink to="https://github.com/kolaczyn">
+              <div className="flex items-center space-x-1">
+                <GithubIcon />
+                <span>github</span>
+              </div>
+            </AppExternalLink>
+            <AppExternalLink to="mailto:kolaczyn@protonmail.com">
+              <div className="flex items-center space-x-1">
+                <MailIcon />
+                <span>mail</span>
+              </div>
+            </AppExternalLink>
+            <AppExternalLink to="https://twitter.com/kolaczyn">
+              <div className="flex items-center space-x-1">
+                <TwitterIcon />
+                <span>twitter</span>
+              </div>
+            </AppExternalLink>
+          </ul>
+        </div>
+      </ContainerBig>
+      <div className="bg-gray-800 h-1" />
+      <div className="bg-gray-900 flex justify-around">
         created by Paweł Kołaczyński &copy; {currentYear}
-      </footer>
-    </ContainerBig>
+      </div>
+    </footer>
   );
 };
 
--- src/components/pages/homePage/components/AboutMe.tsx
+++ src/components/pages/homePage/components/AboutMe.tsx
@@ -4,18 +4,20 @@ import ContainerMedium from "../../../container/ContainerMedium";
 type Props = {};
 
 const AboutMe: React.FC<Props> = () => (
-  <ContainerMedium className="space-y-2">
-    <h1 className="font-bold text-3xl">Hi, I&lsquo;m Paweł</h1>
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum iure
-      soluta magni esse blanditiis temporibus maiores totam tempore, deserunt
-      deleniti.
-    </div>
-    <div className="space-x-2">
-      <AppButton>Read more</AppButton>
-      <AppButton type="secondary">Projects</AppButton>
-    </div>
-  </ContainerMedium>
+  <div className="bg-gray-800 py-6">
+    <ContainerMedium className="space-y-2">
+      <h1 className="font-bold text-3xl">Hi, I&lsquo;m Paweł</h1>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum iure
+        soluta magni esse blanditiis temporibus maiores totam tempore, deserunt
+        deleniti.
+      </div>
+      <div className="space-x-2">
+        <AppButton>Read more</AppButton>
+        <AppButton type="secondary">Projects</AppButton>
+      </div>
+    </ContainerMedium>
+  </div>
 );
 
 export default AboutMe;
