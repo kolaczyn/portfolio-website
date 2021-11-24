2eccb14360eaafc7227f3446c1eb31c0879d0d26
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-11-24 21:56:40 +0100

    fill about me with placeholders

--- src/components/pages/homePage/components/AboutMe.tsx
+++ src/components/pages/homePage/components/AboutMe.tsx
@@ -4,8 +4,13 @@ import ContainerMedium from "../../../container/ContainerMedium";
 type Props = {};
 
 const AboutMe: React.FC<Props> = () => (
-  <ContainerMedium>
-    <h1>home about me</h1>
+  <ContainerMedium className="space-y-2">
+    <h1 className="font-bold text-3xl">Hi, I&lsquo;m Paweł</h1>
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum iure
+      soluta magni esse blanditiis temporibus maiores totam tempore, deserunt
+      deleniti.
+    </div>
     <div className="space-x-2">
       <AppButton>Read more</AppButton>
       <AppButton type="secondary">Projects</AppButton>
