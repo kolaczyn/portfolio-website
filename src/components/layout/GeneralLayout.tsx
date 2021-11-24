2abb65da171c7acf37063572633bbe4ae52ca463
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-11-24 20:50:55 +0100

    fix tailwind

--- src/components/layout/GeneralLayout.tsx
+++ src/components/layout/GeneralLayout.tsx
@@ -4,11 +4,11 @@ import Navbar from "./Navbar";
 type Props = {};
 
 const GeneralLayout: React.FC<Props> = () => (
-  <>
+  <div className="bg-gray-900 text-white">
     <Navbar />
     <div>this is body</div>
     <Footer />
-  </>
+  </div>
 );
 
 export default GeneralLayout;
